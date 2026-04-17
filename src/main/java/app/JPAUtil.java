c2c614b40f9dae7f728809ac05a59428e9ffaddc
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2026-04-17 12:22:20 -0300

    feat: configure jpa persistence unit and entity manager factory

--- src/main/java/app/JPAUtil.java
+++ src/main/java/app/JPAUtil.java
@@ -1,4 +1,14 @@
 package app;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class JPAUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("libraryPU");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
 }
